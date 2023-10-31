94cb15d9973da4fdf5f4afc641462cdb8d53535f
BrObando <brianna.obando@gmail.com>
2023-10-31 12:00:21 -0400

    Buttons

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 console.log('js:loaded')
 
 /*----- constants -----*/
+const colors = ["pink", "blue", "red", "green", "yellow"]
+const gradeColors = ["black", "white", "grey"] // TO DO PUT WHITE IN CSS
 
+/*----- buttons -----*/ //const titleEl = document.querySelector('title');
+
+const pinkButtonEl = document.querySelector("pink");
+const blueButtonEl = document.querySelector("blue");
+const redButtonEl = document.querySelector("red");
+const greenButtonEl = document.querySelector("green");
+const yellowButtonEl = document.querySelector("yellow");
 
   /*----- state variables -----*/
 
